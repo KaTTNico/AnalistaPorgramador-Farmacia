481029670e87f3b87566a024232c313ffd71d7ec
Nicolas <aguirrecampi.nicolas@gmail.com>
2019-05-17 23:43:14 -0300

    SE ARREGLAN COSAS DEL MER Y SE AGREGAN TABLAS EN SCRIPT

--- Script/script.sql
+++ Script/script.sql
@@ -35,7 +35,7 @@ go
 
 --create tables
 create table usuario(
-documento int primary key not null,
+documento int primary key,
 nombreUsuario varchar(15) unique not null,
 pass varchar(7) not null check(pass like '[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9][0-9]'),
 nombreCompleto varchar(25) not null
@@ -50,6 +50,40 @@ go
 
 create table empleado(
 documento int foreign key references usuario(documento) not null,
-horaInicio varchar(5) not null,
-horaFin varchar(5) not null
+horaInicio date not null,
+horaFin date not null
 );
+go
+
+create table horasExtraEmpleado(
+documento int foreign key references empleado(documento) not null,
+minutos int not null
+);
+go
+
+create table farmaceutica(
+nombre varchar(50) primary key,
+direccionFiscal varchar(100) not null,
+telefono varchar(7) not null,
+correoElectronico varchar(25) not null
+);
+go
+
+create table medicamento(
+nombreFarmaceutica foreign key references farmaceutica(nombre) not null,
+codigo varchar(50) primary key,
+nombre varchar(8) not null,
+descripcion varchar(100) not null,
+precio money not null,
+tipo varchar(13) not null check(tipo='CARDIOLOGICO' or tipo='DIABETICOS' or tipo='OTROS'),
+stock int not null
+);
+
+create table pedido(
+numero int primary key,
+fechaRealizado date not null,
+direccionEntrega varchar(100) not null,
+estado int not null
+);
+go
+
